fc5534160f5a952d112b4a8050277335d03c8c12
HDUY102 <leduy020702@gmail.com>
2025-10-28 18:11:27 +0700

    Create and deployment zustand

---
+++ zustand/useTodoStore.ts
@@ -0,0 +1,67 @@
+import { initialTasks } from "@/data/initialTask";
+import { Task, TypeFilter } from "@/generics/TodoGeneric";
+import { create } from "zustand";
+
+interface TodoState{
+    tasks: Task[]
+    currentFilter: TypeFilter
+
+    onFilterChangeHandler: (filter: TypeFilter) => void
+    onDeletedDoneTasksHandler: () => void
+    onDeletedAllTasksHandler: () => void
+    onToggleCompletedHandler: (id: number) => void
+    onEditedTaskHandler: (id: number, title: string) => void
+    onDeletedTaskHandler: (id:number) => void
+    onSubmitTaskHandler: (title: string) => void
+}
+
+export const useTodoStore = create<TodoState>((set,get) => ({
+    tasks: initialTasks,
+    currentFilter: 'All',
+
+    onFilterChangeHandler: (filter) => set({ currentFilter: filter }),
+
+    onDeletedDoneTasksHandler: () => {
+        set((state) => ({
+            tasks: state.tasks.map(task =>
+                (task.isCompleted && !task.isDeleted) ? { ...task, isDeleted: true } : task)
+        }))
+    },
+
+    onDeletedAllTasksHandler: () => {
+        set((state) => ({
+            tasks: state.tasks.map(task =>
+                !task.isDeleted ? { ...task, isDeleted: true } : task)
+        }))
+    },
+
+    onToggleCompletedHandler: (id) => {
+        set((state) => ({
+            tasks: state.tasks.map(task =>
+                task.id === id ? { ...task, isCompleted: !task.isCompleted } : task)
+        }))
+    },
+
+    onEditedTaskHandler: (id, title) => {
+        set((state) => ({
+            tasks: state.tasks.map(task =>
+                task.id === id ? { ...task, title: title } : task)
+        }))
+    },
+
+    onDeletedTaskHandler: (id) => {
+        set((state) => ({
+            tasks: state.tasks.map(task =>
+                task.id === id ? { ...task, isDeleted: true } : task)
+        }))
+    },
+
+    onSubmitTaskHandler: (title) => {
+        set((state) => ({
+            tasks: [
+                ...state.tasks,
+                { id: state.tasks.length+1, title: title, isCompleted: false, isDeleted: false }
+            ]
+        }))
+    },
+}))
